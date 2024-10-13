ae8013fd955639bea7f28b70842f26b3ffb56e56
Bladimir Ramos <bladi.ram@gmail.com>
2024-10-13 01:14:07 -0400

    Implementado update y delete de role

--- core/calibus/urls.py
+++ core/calibus/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('role/list/', RoleListView.as_view(), name='role_list'),
     path('role/list2/', role_list, name='role_list2'),
     path('role/add/', RoleCreateView.as_view(), name='role_create'),
+    path('role/edit/<int:pk>', RoleUpdateView.as_view(), name='role_update'),
+    path('role/delete/<int:pk>', RoleDeleteView.as_view(), name='role_delete'),
 ]
--- core/calibus/views/role/views.py
+++ core/calibus/views/role/views.py
@@ -2,7 +2,7 @@ from django.http import JsonResponse, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_exempt
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
 
@@ -83,4 +83,50 @@ class RoleCreateView(CreateView):
         context['entity'] = 'Roles'
         context['list_url'] = reverse_lazy('calibus:role_list')
         context['action'] = 'add'
+        return context
+
+
+class RoleUpdateView(UpdateView):
+    model = Role
+    form_class = RoleForm
+    template_name = 'role/create.html'
+    success_url = reverse_lazy('calibus:role_list')
+
+    def dispatch(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        return super().dispatch(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        data = {}
+        try:
+            action = request.POST['action']
+            if action == 'edit':
+                # rol = self.get_object()
+                form = self.get_form()
+                data = form.save()
+            else:
+                data['error'] = 'No ha ingresado ninguna opción'
+        except Exception as e:
+            data['error'] = str(e)
+        return JsonResponse(data)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Editar Rol'
+        context['entity'] = 'Roles'
+        context['list_url'] = reverse_lazy('calibus:role_list')
+        context['action'] = 'edit'
+        return context
+
+
+class RoleDeleteView(DeleteView):
+    model = Role
+    template_name = 'role/delete.html'
+    success_url = reverse_lazy('calibus:role_list')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Eliminar Rol'
+        context['entity'] = 'Roles'
+        context['list_url'] = reverse_lazy('calibus:role_list')
         return context
